19cb602d642c097bdaa920c3dc20013c06e5b9ba
admin <admin@vraney-mbp.thelamplighterschool.org>
2017-12-06 11:00:53 -0600

    close

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,3 +1,4 @@
+
 //array
 var players = ["Tim Duncan",
 			"Kawhi Leonard",
@@ -5,13 +6,12 @@ var players = ["Tim Duncan",
 			"Tony Parker",
 			"Gregg Popovich",
 			"David Robinson",
-			"San Antonio Spurs Coyote",
+			"Spurs Coyote",
 			"Patty Mills",
 			"Danny Green",
 			"Boris Diaw"];
 
-
-//displays player data
+//displays player buttons
 function renderButtons() {
 //delete players before adding new ones
 	$("#buttons-view").empty();
@@ -25,38 +25,7 @@ function renderButtons() {
 		$("#buttons-view").append(a);
 		
 	}
-
-	$(".player").on("click", function() {
-	$("#gifs-appear-here").empty();
-	var playerGif = $(this).attr("data-name");
-	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        playerGif + "&api_key=dc6zaTOxFJmzC&limit=10";
-   	$.ajax({
-          url: queryURL,
-          method: "GET"
-        })
-        // After data comes back from the request
-        .done(function(response) {
-        	console.log(queryURL)
-        	var results = response.data;
-
-        	for (var i=0; i < results.length; i++) {
-        		var playerDiv = $("<div>");
-        		var p = $("<p>").text("Rating: " + results[i].rating);
-        		var playerImage = $("<img>");
-        		playerImage.attr("src", results[i].images.fixed_height.url);
-        		playerDiv.append(p);
-           		playerDiv.append(playerImage);
-           		$("#gifs-appear-here").append(playerDiv);
-        	}
-        })
-
-})
-
 };
-
-
-//click event on buttons
 
 //click function
 $("#add-player").on("click", function(event) {
@@ -71,3 +40,59 @@ $("#add-player").on("click", function(event) {
 });
 
 renderButtons();
+
+//function to display gifs
+function displayGifs() {
+	//sets variable that 
+	var players = $(this).attr("data-name");
+	//defines our apikey
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        players + "&api_key=dc6zaTOxFJmzC&limit=10";
+    //ajax call
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    })
+    .done(function(response) {
+    	// empties div
+        $("gifs-appear-here").empty(); 
+        var results = response.data; //shows results of gifs
+        //for when there's no gif from the added button
+        if (results == ""){
+          alert("ain't no gifs here");
+        }
+        for (var i=0; i<results.length; i++){
+            //creates div for where to put gif
+            var gifDiv = $("<div>"); 
+            gifDiv.addClass("gifDiv");
+            // pulling rating of gif
+            var gifRating = $("<p>").text("Rating: " + results[i].rating);
+            gifDiv.append(gifRating);
+            // pulling gif
+            var gifImage = $("<img>");
+            gifImage.attr("src", results[i].images.fixed_height_small_still.url); // still image stored into src of image
+            gifImage.attr("data-still",results[i].images.fixed_height_small_still.url); // still image
+            gifImage.attr("data-animate",results[i].images.fixed_height_small.url); // animated image
+            gifImage.attr("data-state", "still"); // set the image state
+            gifImage.addClass("image");
+            gifDiv.append(gifImage);
+            // pulling still image of gif
+            // adding div of gifs to gifsView div
+            $("#gifs-appear-here").prepend(gifDiv);
+        }
+    });
+};
+
+//click function for display gifs
+$(document).on("click", ".player", displayGifs);
+//click function to change gifs from still to working
+$(document).on("click", ".image", function(){
+    var state = $(this).attr('data-state');
+    if ( state == 'still'){
+        $(this).attr('src', $(this).data('animate'));
+        $(this).attr('data-state', 'animate');
+    }else{
+        $(this).attr('src', $(this).data('still'));
+        $(this).attr('data-state', 'still');
+    }
+});
